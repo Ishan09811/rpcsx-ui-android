8deee2ae6e16d0a2c2c0313ad840e31482383e4f
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-05-14 13:12:31 +0530

    Update AppNavHost.kt

--- app/src/main/java/net/rpcsx/ui/navigation/AppNavHost.kt
+++ app/src/main/java/net/rpcsx/ui/navigation/AppNavHost.kt
@@ -16,6 +16,7 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -60,6 +61,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
