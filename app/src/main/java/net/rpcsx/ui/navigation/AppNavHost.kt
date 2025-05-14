fca7e924cad05cd7703aff982a79368e072a15ff
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-05-14 19:29:46 +0530

    Update AppNavHost.kt

--- app/src/main/java/net/rpcsx/ui/navigation/AppNavHost.kt
+++ app/src/main/java/net/rpcsx/ui/navigation/AppNavHost.kt
@@ -66,6 +66,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.vector.ImageVector
