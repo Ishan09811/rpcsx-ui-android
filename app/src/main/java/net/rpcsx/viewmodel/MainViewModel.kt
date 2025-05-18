d0eba3c095c9fa63364b2ebed621d0d64b103d78
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-05-18 22:49:36 +0530

    Update MainViewModel.kt

--- app/src/main/java/net/rpcsx/viewmodel/MainViewModel.kt
+++ app/src/main/java/net/rpcsx/viewmodel/MainViewModel.kt
@@ -10,7 +10,14 @@ class MainViewModel : ViewModel() {
     private val _isBottomNavigationVisible = MutableStateFlow(true)
     val isBottomNavigationVisible = _isBottomNavigationVisible.asStateFlow()
 
+    private val _itemUsageMap = mutableStateMapOf<String, Long>() // key -> last used timestamp
+    val itemUsageMap: Map<String, Long> get() = _itemUsageMap    
+
     fun setBottomNavigationVisibility(visible: Boolean) {
         _isBottomNavigationVisible.value = visible
+    }
+
+    fun recordItemUsage(key: String) {
+        _itemUsageMap[key] = System.currentTimeMillis()
     }
 }
