f03a31752d9ff469bab10813cd9226b8ce201bbf
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-05-19 09:47:45 +0530

    Update MainViewModel.kt

--- app/src/main/java/net/rpcsx/viewmodel/MainViewModel.kt
+++ app/src/main/java/net/rpcsx/viewmodel/MainViewModel.kt
@@ -1,7 +1,6 @@
 
 package net.rpcsx.viewmodel
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,14 +9,16 @@ class MainViewModel : ViewModel() {
     private val _isBottomNavigationVisible = MutableStateFlow(true)
     val isBottomNavigationVisible = _isBottomNavigationVisible.asStateFlow()
 
-    private val _itemUsageMap = mutableStateMapOf<String, Long>()
-    val itemUsageMap: Map<String, Long> get() = _itemUsageMap    
+    private val _itemUsageMap = MutableStateFlow<Map<String, Long>>(emptyMap())
+    val itemUsageMap: StateFlow<Map<String, Long>> = _itemUsageMap    
 
     fun setBottomNavigationVisibility(visible: Boolean) {
         _isBottomNavigationVisible.value = visible
     }
 
     fun recordItemUsage(key: String) {
-        _itemUsageMap[key] = System.currentTimeMillis()
+        _itemUsageMap.value = _itemUsageMap.value.toMutableMap().apply {
+            this[key] = System.currentTimeMillis()
+        }
     }
 }
